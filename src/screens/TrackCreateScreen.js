52021c9e9a231519be23446db667852027ca0c85
charles mancuso <charlesmancuso66@yahoo.com>
2021-01-10 17:45:28 -0800

    Add location services

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, SafeAreaView } from 'react-native';
 import { Text } from 'react-native-elements';
+import { requestPermissionsAsync } from 'expo-location';
 import Map from '../components/Map';
 
 const TrackCreateScreen = () => {
+  const [err, setErr] = useState(null);
+
+  const startWatching = async () => {
+    try {
+      const { granted } = await requestPermissionsAsync();
+      if (!granted) {
+        throw new Error('Location permission not granted');
+      }
+    } catch (e) {
+      setErr(e);
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+  
   return (
     <SafeAreaView forceInset={{ top: 'always '}}>
       <Text h2>Create a Track</Text>
       <Map />
+      {err ? <Text>Please enable location services</Text> : null}
     </SafeAreaView>
   )
 };
